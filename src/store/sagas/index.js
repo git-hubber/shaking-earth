5d672aae19275d535416b435b2190466767e0d68
Martin S <info@mgdsventures.com>
2019-03-13 22:44:46 +1100

    refactor to use Redux Saga instead of Redux Thunk

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,4 +1,4 @@
-import { takeEvery, all } from 'redux-saga/effects';
+import { takeEvery, takeLatest, all } from 'redux-saga/effects';
 
 import * as types from '../types.js';
 import { fetchEarthquakesSaga, filterEarthquakesSaga } from './earthquakeSagas';
@@ -6,6 +6,7 @@ import { fetchEarthquakesSaga, filterEarthquakesSaga } from './earthquakeSagas';
 export function* watchEarthquakes() {
   /**
    * takeEvery - listen for certain action types -> and execute certain sagas when they occur:
+   * takeLatest - take only the latest (cancels any previous saga that's still running)
    *
    * INITIATE_FETCH_EARTHQUAKES -> fetchEarthquakesSaga
    * OR
@@ -14,6 +15,6 @@ export function* watchEarthquakes() {
 
   yield all([
     takeEvery(types.INITIATE_FETCH_EARTHQUAKES, fetchEarthquakesSaga),
-    takeEvery(types.INITIATE_FILTER_EARTHQUAKES, filterEarthquakesSaga),
+    takeLatest(types.INITIATE_FILTER_EARTHQUAKES, filterEarthquakesSaga),
   ]);
 }
